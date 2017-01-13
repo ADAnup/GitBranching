562d0db197a00bc63726e27912adef1dd58b4b4e
anup.ascratech <anup@ascratech.in>
2017-01-13 16:53:57 +0530

    Text change.

--- app/src/main/java/com/app/gitbranching/MainActivity.java
+++ app/src/main/java/com/app/gitbranching/MainActivity.java
@@ -2,12 +2,17 @@ package com.app.gitbranching;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvHello;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvHello = (TextView) findViewById(R.id.tvHello);
+        tvHello.setText("Changing from Activity");
     }
 }

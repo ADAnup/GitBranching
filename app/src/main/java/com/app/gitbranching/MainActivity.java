9c8540f5d5dec89e27818381145251b82cddad49
anup.ascratech <anup@ascratech.in>
2017-01-13 17:02:36 +0530

    Button Click

--- app/src/main/java/com/app/gitbranching/MainActivity.java
+++ app/src/main/java/com/app/gitbranching/MainActivity.java
@@ -2,17 +2,29 @@ package com.app.gitbranching;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvHello;
+    private ImageButton ibButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         tvHello = (TextView) findViewById(R.id.tvHello);
+        ibButton = (ImageButton) findViewById(R.id.ibButton);
         tvHello.setText("Changing from Activity");
+
+        ibButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Button Clicked..", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
